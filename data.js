5289a750835ee0e17f2402c1860b8b0f62a47220
devauntea <devauntea.thompson@csedge.org>
2018-10-06 15:46:15 -0400

    complited challenge 1

--- data.js
+++ data.js
@@ -1,5 +1,25 @@
 // Example
-let message = "Hello World!";
-console.log(message);
+constant READLINE = ("readline-sync")
 
 // Type your code below!
+let num1 = 132;
+let num0 = 911;
+let num2 = 963;
+let tHEDOOR = "leave"
+let gLOBWARMING = "real"
+let iLLUMINATI = "confirmed"
+let nitefort = true;
+let shellshockers = false;
+let scrumpcuhs;
+console.log(num1);
+console.log(num2);
+console.log(tHEDOOR);
+console.log(gLOBWARMING);
+console.log(iLLUMINATI);
+console.log(nitefort);
+console.log(shellshockers);
+console.log(scrumpcuhs);
+console.log(message);
+
+let userinput = READLINE.question("dodo")
+console.log('you typed: ${userinput}');
