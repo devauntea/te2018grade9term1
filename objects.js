2fd80a188ee17d10bc04b101adb11f2f8b8fad4f
devauntea <devauntea.thompson@csedge.org>
2018-10-20 12:50:46 -0400

    crime brought to justice

---
+++ objects.js
@@ -0,0 +1,17 @@
+let spy = {
+	firstName: "burnt",
+	lastName: "chicken.nuget",
+	nickName:"Good-Day.Sir ",
+	catchphrase: "This day is juat getting better and better",
+	archenemy: "mr.scrumpstious",
+};
+
+let villian = {
+	firstName: "unknown",
+	lastName: "unknown",
+	nickName: "mr.scrumpstious",
+	catchphrase: "WOW! how scrumpstious",
+	greatstCrime: "farting on cats",
+};
+
+console.log(`Hi I am ${spy.firstName} ${spy.lastName}. but you can call me ${spy.nickName}. now lets get to the point. I will end your days of ${villian.greatstCrime}. not today ${spy.nickName} I knew you would try and stop me so i stole 25% of the earths cats and put them in a camber that can only be accesed by my farts. damn you and your evil ways ${villian.nickName} i will put a end to this now! ${villian.catchphrase} of you to say. you think you can stop me. ${spy.catchphrase} isn't it. to be continued.`)
